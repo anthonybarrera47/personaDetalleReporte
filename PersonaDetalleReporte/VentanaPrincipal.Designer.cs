54cc7c9cb3a3a5640b6db88974eef2ebba27fc7e
Anthony Manuel Barrera Hidalgo <anthony_barrera@ucne.edu.do>
2018-10-19 19:05:30 -0400

    Creacion De las Carpetas

--- PersonaDetalleReporte/VentanaPrincipal.Designer.cs
+++ PersonaDetalleReporte/VentanaPrincipal.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace PersonaDetalleReporte
 {
-    partial class Form1
+    partial class VentanaPrincipal
     {
         /// <summary>
         /// Required designer variable.
@@ -28,10 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // VentanaPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.IsMdiContainer = true;
+            this.Name = "VentanaPrincipal";
+            this.Text = "Ventna Principal";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- PersonaDetalleReporte/VentanaPrincipal.cs
+++ PersonaDetalleReporte/VentanaPrincipal.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace PersonaDetalleReporte
 {
-    public partial class Form1 : Form
+    public partial class VentanaPrincipal : Form
     {
-        public Form1()
+        public VentanaPrincipal()
         {
             InitializeComponent();
         }
